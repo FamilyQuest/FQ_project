7af1edc4813011d5108efadb2c63491047889bf4
eido peretz <eido2488@gmail.com>
2024-02-15 10:48:07 +0200

    FAM-24

---
+++ components/avatar.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    Avatar: {
+        width: "90%",
+        height: "90%",
+    },
+
+});
+
+export default styles;
