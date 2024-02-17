a06c032ade43b5318ed9578d2b498cdd84643d23
eido peretz <eido2488@gmail.com>
2024-02-17 13:33:34 +0200

    Init

--- components/Page-Format/Header.jsx
+++ components/Page-Format/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
+import { View, Image, TouchableOpacity,StatusBar } from 'react-native';
 
 import styles from './header.style';
 
 function Header() {
   return (
     <View>
+        <StatusBar backgroundColor={"#A4E7DD"} />
         <View style={styles.topScreenContainer}><Image style={styles.headerImg} source={require('../../assets/General/Header.png')} /></View>
         <View style={styles.header}>
             <TouchableOpacity style={styles.headerButton} onPress={() => { }}>
