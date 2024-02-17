d102fc6a53013cb4c703561548dea8dc4b723499
eido peretz <eido2488@gmail.com>
2024-02-17 13:33:34 +0200

    Init

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
-import { SafeAreaView, StatusBar } from 'react-native';
-import AvatarCreationForm from './components/AvatarCreationForm';
+import PageTemplate from './components/PageTemplate';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <StatusBar backgroundColor="#A4E7DD" barStyle="dark-content" />
-      <AvatarCreationForm />
-    </SafeAreaView>
+    <PageTemplate />
   );
 }
