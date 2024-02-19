801f4564a7011f0bf63eb878a92b9bdd6302bc8b
eido peretz <eido2488@gmail.com>
2024-02-19 14:16:19 +0200

    FAM-32

--- components/DataBase/dbConnectionTasks.js
+++ components/DataBase/dbConnectionTasks.js
@@ -1,25 +1,23 @@
 import { useState, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
-import firebase from 'firebase/compat/app';
-import { getDatabase, ref, get, set, onValue } from 'firebase/database';
-import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGINGSENDER_ID, APP_ID, MEASUREMETN_ID } from '@env';
+import { getDatabase, ref, get } from 'firebase/database';
+import { func } from 'prop-types';
 
 const dbConnectionTasks = () => {
   const [tasks, setTasks] = useState([]);
+
   useEffect(() => {
     const firebaseConfig = {
-      apiKey: API_KEY,
-      authDomain: AUTH_DOMAIN,
-      projectId: PROJECT_ID,
-      storageBucket: STORAGE_BUCKET,
-      messagingSenderId: MESSAGINGSENDER_ID,
-      appId: APP_ID,
-      measurementId: MEASUREMETN_ID,
+        apiKey: "AIzaSyAEhAIE9OBu4EIpaW2Xff-wuzNVYrx6Iow",
+        authDomain: "familyquest-43f50.firebaseapp.com",
+        projectId: "familyquest-43f50",
+        storageBucket: "familyquest-43f50.appspot.com",
+        messagingSenderId: "435404741742",
+        appId: "1:435404741742:web:e964f4ae7470e38fa11a7f",
+        measurementId: "G-VG2Z4F1K75",
     };
 
-    if (!firebase.apps.length) {
-      const app = initializeApp(firebaseConfig);
-    }
+    const app = initializeApp(firebaseConfig);
     const db = getDatabase();
 
     const tasksRef = ref(db, 'tasks/');
@@ -39,25 +37,6 @@ const dbConnectionTasks = () => {
       .catch((error) => {
         console.error(error);
       });
-
-    // Add listener for changes in tasks data
-    const unsubscribe = onValue(tasksRef, (snapshot) => {
-      if (snapshot.exists()) {
-        const tasksData = snapshot.val();
-        const tasksArray = Object.keys(tasksData).map((key) => ({
-          id: key,
-          ...tasksData[key],
-        }));
-        setTasks(tasksArray);
-      } else {
-        console.log("No data available");
-      }
-    });
-
-    // Clean up listener on component unmount
-    return () => {
-      unsubscribe(); // Remove the listener
-    };
   }, []);
 
   function getTasksByUserId(userId) {
@@ -102,46 +81,10 @@ const dbConnectionTasks = () => {
       return undefined;
     }
   }
+  
 
-  function getTasksByAdminId(adminId) {
-    const foundTasks = tasks.filter(task => task.admin_id === adminId);
-    if (foundTasks.length > 0) {
-      return foundTasks;
-    } else {
-      console.log("No tasks found for user with ID " + adminId);
-      return [];
-    }
-  }
 
-  function getUserIdByTaskId(taskId) {
-    const foundTask = tasks.find(task => task.id === taskId);
-    if (foundTask) {
-      return foundTask.user_id;
-    } else {
-      console.log("No task found with ID " + taskId);
-      return undefined;
-    }
-  }
-
-  function updateStatusTaskByTaskIdAndUserId(userId, taskId, newStatus) {
-    const updatedTasks = tasks.map(task => {
-      if (task.user_id === userId && task.id === taskId) {
-        return { ...task, status: newStatus };
-      } else {
-        return task;
-      }
-    });
-
-    setTasks(updatedTasks); // Update local state
-
-    const db = getDatabase();
-    const taskRef = ref(db, `tasks/${taskId - 1}/status`);
-    set(taskRef, newStatus) // Update only the status in the database
-      .then(() => console.log("Task status updated successfully"))
-      .catch((error) => console.error("Error updating task status:", error));
-  }
-
-  return { tasks, getTasksByUserId, updateStatusTaskByTaskIdAndUserId, getTasksByAdminId, getUserIdByTaskId };
+  return { tasks ,getTasksByUserId };
 }
 
 export default dbConnectionTasks;
