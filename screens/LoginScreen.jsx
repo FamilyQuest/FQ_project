2f74913d88ac2ea4a924cde2477dfddf914e3929
Naim <naimreem1@gmail.com>
2024-03-04 22:56:25 +0200

    FAM-62
    Remove remember me and forget me
    Remove Background image
    Remove small logo

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import { ImageBackground, I18nManager} from 'react-native';
-
+import { ImageBackground, I18nManager,View} from 'react-native';
 I18nManager.forceRTL(false);
 I18nManager.allowRTL(false);
 
-import Header from '../components/Page-Format/Header';
+import Header from '../components/Page-Format/LoginHeader';
 import Login from '../components/Login/LogIn';
 
 const LoginScreen = ({navigation}) => {
 
   return (
-    <ImageBackground
-      style={{height: "100%",width: "100%"}}
-      source={require('../assets/Backgrounds/bg1.png')}>
+    <View>
       <Header navigation={navigation} visible={true}/>
       <Login navigation={navigation}/>
-    </ImageBackground>
+    </View>
 
   );
 };
