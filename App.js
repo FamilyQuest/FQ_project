93b0bb9ad83e95f80e292b4e0695320260bcc702
eido peretz <eido2488@gmail.com>
2024-03-28 18:49:41 +0200

    Merge Fix 2

--- App.js
+++
@@ -1,9 +0,0 @@
-import { View } from 'react-native';
-import PageTemplate from './components/PageTemplate';
-export default function App() {
-  return (
-    <PageTemplate> 
-      
-    </PageTemplate>
-  );
-}
